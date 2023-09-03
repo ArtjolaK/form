0eaba3ca6c65c8d6446767350694649888e53a04
ArtjolaK <koltrakaartjola@gmail.com>
2023-09-03 17:04:15 +0200

    first commit

--- src/form.js
+++ src/form.js
@@ -1,10 +1,4 @@
 import React, { useState } from "react";
-// JS
-// const input = document.getElementById('myText');
-// const inputValue = input.value
-// React
-// value, onChange
-// dynamic object keys
 
 const CreatForm = () => {
   const [people, setPeople] = useState([]);
